c856d2e5a9e02140db5999ea2fdddaa67d1e49fd
mcadzz266 <141048889+mcadzz266@users.noreply.github.com>
2024-08-29 08:58:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "21658931")
+    API_HASH = environ.get("API_HASH", "7c153d294a7a92df6781409b6ae7d3c7")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7418683918:AAHU3TmqlLzaHmT-2EqGHtJ8t8I8OTZG1Z4').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://demobots:demobots@cluster0.yymbntq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002191998268'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
